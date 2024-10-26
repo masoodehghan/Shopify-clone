cd23e2e5be884c37a020003182f6df17ec757bb4
masood <masood8204@gmail.com>
2024-10-27 00:02:58 +0330

    complete slider

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,12 @@ const verticalMenuDropDownBlcok = $.querySelector('.block_content')
 alphaVerticalMenu.addEventListener('mouseover', (e) => {
     e.preventDefault()
     verticalMenuDropDownBlcok.style.display = 'block';
-    console.log(verticalMenuDropDownBlcok);
 })
 
 alphaVerticalMenu.addEventListener('mouseout', (e) => {
     e.preventDefault()
     
     verticalMenuDropDownBlcok.style.display = 'none';
-    console.log(verticalMenuDropDownBlcok);
 })
+
+
