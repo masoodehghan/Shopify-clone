e43f2f4fbf928c3625ed2224e296ffa0893a2992
masood <masood8204@gmail.com>
2024-10-27 23:03:33 +0330

    add product carousel

---
+++ components/CarouselItem/CarouselItem.js
@@ -0,0 +1,8 @@
+class CarouselItem extends HTMLElement {
+    constructor() {
+        this.innerHTML = "THIS IS COMPONNET"
+    }
+}
+
+
+export default CarouselItem;
--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
+import { CarouselItem} from '../components/CarouselItem/CarouselItem';
+
 const $ = document;
+
+
+customElements.define('carousel-item', CarouselItem);
 
 const alphaVerticalMenu = $.querySelector('#alpha-vertical-menu');
 const verticalMenuDropDownBlcok = $.querySelector('.block_content')
