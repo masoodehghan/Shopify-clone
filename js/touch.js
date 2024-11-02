ade60cf6a9836f319d052ca6209a1b772cfe24c4
masood <masood8204@gmail.com>
2024-11-03 00:09:31 +0330

    complete responsive

--- js/touch.js
+++ js/touch.js
@@ -3,33 +3,41 @@ const $ = document;
 const currencyBlock = $.querySelector("#currency_block");
 const dropdownCurrency = currencyBlock.lastElementChild;
 
-const userBlock = $.querySelector('.header_user_info');
+const userBlock = $.querySelector(".header_user_info");
 const dropDownUserBlock = userBlock.lastElementChild;
+const footerQuickLink = $.querySelectorAll(".footer-block.block");
 
 currencyBlock.firstElementChild.addEventListener("click", (e) => {
   e.stopPropagation();
-  displayDropDown(dropdownCurrency);
-
-});
-
-$.querySelector("#page-content").addEventListener("click", (e) => {
-    e.stopPropagation();
+  if (dropdownCurrency.style.visibility) {
     hideDropDown(dropdownCurrency);
-    hideDropDown(dropDownUserBlock);
+  } else {
+    displayDropDown(dropdownCurrency);
+  }
 });
 
-userBlock.firstElementChild.addEventListener('click', (e) => {
-    e.stopPropagation();
+document.body.addEventListener("click", (e) => {
+  e.stopPropagation();
+  hideDropDown(dropdownCurrency);
+  hideDropDown(dropDownUserBlock);
+});
+
+userBlock.firstElementChild.addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (dropDownUserBlock.style.visibility) {
+    hideDropDown(dropDownUserBlock);
+  } else {
     displayDropDown(dropDownUserBlock);
-})
+  }
+});
 
 function displayDropDown(el) {
-    el.style.transform = "scale(1)";
-    el.style.visibility = "visible";
+  el.style.transform = "scale(1)";
+  el.style.visibility = "visible";
 }
 
 function hideDropDown(el) {
-    el.style = "";
+  el.style = "";
 }
 
 
