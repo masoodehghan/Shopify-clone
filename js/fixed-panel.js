7a5e951da3a90900b36ea74f39b7c675df198fb6
masood <masood8204@gmail.com>
2024-11-04 22:33:45 +0330

    complete fixed panel and fix bugs in header

--- js/fixed-panel.js
+++ js/fixed-panel.js
@@ -1,14 +1,32 @@
-const fixedPanel = $.getElementById('j-floor-fixed-panel');
+const fixedPanel = $.getElementById("j-floor-fixed-panel");
 
-window.addEventListener('scroll', () => {
-    centerColumnWidth = $.getElementById('center_column').offsetWidth;
-    if(scrollY > 966) {
-        fixedPanel.classList.add('floor-fixed');
-        fixedPanel.style.position = 'fixed';
-        console.log(innerWidth - centerColumnWidth);    
-        fixedPanel.style.left = `${innerWidth / 2 - 665}px`
-    } else {
-        fixedPanel.classList.remove('floor-fixed');
+window.addEventListener("scroll", () => {
+  centerColumnWidth = $.getElementById("center_column").offsetWidth;
+  if (scrollY > 966 && scrollY < 5530) {
+    fixedPanel.classList.add("floor-fixed");
+    fixedPanel.style.position = "fixed";
+    fixedPanel.style.left = `${innerWidth / 2 - 665}px`;
+
+    let categoryTab;
+    for (categoryTab of $.querySelectorAll(".categoryTabs")) {
+      isScrolledToElement(categoryTab);
     }
-})
 
+  } else {
+    fixedPanel.classList.remove("floor-fixed");
+  }
+});
+
+const topWindow = 0;
+const botttomOfWindow = topWindow + document.body.scrollHeight;
+
+function isScrolledToElement(el) {
+  elParent = el.parentElement.parentElement;
+  let bottomOfEl = elParent.offsetTop + elParent.scrollHeight;
+  let fixedItem = $.querySelector(`.floor-nav-list a[href="#${el.id}"]`);
+  if (elParent.offsetTop < scrollY && bottomOfEl > scrollY ) {
+    fixedItem.classList.add('active');
+  } else {
+    fixedItem.classList.remove('active')
+  }
+}
